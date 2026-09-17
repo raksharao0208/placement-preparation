c8bae2c5942e2f200de11dd19d2d997cda7632a1
raksha <raksharao0208@gmail.com>
2026-09-17 20:06:36 +0530

    Add Day 1 Python practice

---
+++ Python/Day-01/day1.py
@@ -0,0 +1,48 @@
+# Program 1
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("Sum of the two numbers: ", a + b)
+print("Difference of two numbers: ", a - b)
+print("Product of two numbers: ", a * b)
+print("Division of two numbers: ", a / b)
+print("Remainder of two numbers: ", a % b)
+
+
+# Program 2
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+cgpa = float(input("Enter your CGPA: "))
+
+print("Name : ", name)
+print("Age : ", age)
+print("CGPA : ", cgpa)
+
+
+# Program 3
+import math
+
+r = int(input("Enter radius of a circle: "))
+
+area = math.pi * r * r
+
+print("Area of circle is: ", area)
+
+
+# Program 4
+x = int(input("Enter a number: "))
+
+print("Square of the number: ", x * x)
+print("Cube of the number: ", x * x * x)
+
+
+# Program 5
+a = int(input("Enter marks of subject 1: "))
+b = int(input("Enter marks of subject 2: "))
+c = int(input("Enter marks of subject 3: "))
+
+total = a + b + c
+average = total / 3
+
+print("Total marks: ", total)
+print("Average marks: ", average)
